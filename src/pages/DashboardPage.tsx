88ad85f4b50a98bf6ab525e95f8b2990937bf8f4
Eugene <eugenelevkovych@yahoo.com>
2025-08-20 15:37:43 +0300

    add sales

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -12,21 +12,20 @@ const DashboardPage = () => {
   return (
     <>
       <Header />
-      <div className="min-h-screen p-8">
-        <div className="max-w-4xl mx-auto">
-          <div className="flex justify-between items-center mb-8">
-            <h1 className="text-3xl font-bold text-grey1">Dashboard</h1>
-            <button
-              onClick={handleLogout}
-              className="px-6 py-3 bg-blue2 text-white rounded-xl font-semibold hover:bg-blue1 transition-colors"
-            >
-              Exit
-            </button>
-          </div>
+      <div className="p-8 bg-grey8">
+        <div className="flex justify-between items-center mb-2">
+          <h1 className="text-3xl font-bold text-grey1">Dashboard</h1>
 
-          <div className="bg-white rounded-xl p-6 shadow-lg">
-            <h2 className="text-xl font-semibold mb-4">Product</h2>
-          </div>
+          <button
+            onClick={handleLogout}
+            className="px-6 py-3 bg-blue2 text-white rounded-xl font-semibold hover:bg-blue1 transition-colors"
+          >
+            Exit
+          </button>
+        </div>
+        <p className="mb-5.5">dashboard</p>
+        <div className="bg-white rounded-3xl p-5">
+          <h3 className="mb-4">Sales Target</h3>
         </div>
       </div>
     </>
