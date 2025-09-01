e4adc0a4e1a40c5e8b2b75847d713b78c6689d7c
Eugene <eugenelevkovych@yahoo.com>
2025-09-01 18:17:18 +0300

    add notif

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,20 @@ import Input from "./Input";
 
 const Header = () => {
   return (
-    <div className="px-8 py-[1.6rem]">
+    <div className="flex justify-between px-8 py-[1.6rem]">
       <Input />
+      <div className="flex gap-4">
+        <div className="flex justify-center items-center rounded-lg size-9 bg-gray13">
+          <svg className="size-6 stroke-gray7 fill-transparent">
+            <use href="./sprite.svg#icon-mail"></use>
+          </svg>
+        </div>
+        <div className="flex justify-center items-center rounded-lg size-9 bg-gray13">
+          <svg className="size-6 stroke-gray7 fill-transparent">
+            <use href="./sprite.svg#icon-notifications"></use>
+          </svg>
+        </div>
+      </div>
     </div>
   );
 };
