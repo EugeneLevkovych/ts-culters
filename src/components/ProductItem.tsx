ebf0544290054abee359bf2934756dfbb9e82cf8
Eugene <eugenelevkovych@yahoo.com>
2025-09-04 13:15:41 +0300

    test 2

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -10,22 +10,71 @@ const ProductItem = ({ product }: ProductProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <>
-      <div className="flex border-b border-gray13 hover:bg-gray12 transition-colors p-4 lg:p-3">
+    <div className="flex justify-between border-b border-gray13">
+      <div className="flex p-4 lg:p-3">
         <div className="mr-3">
           <input className="accent-blue2 cursor-pointer" type="checkbox" />
         </div>
 
-        <div className="flex-1 flex items-center gap-2 min-w-0">
-          <img className="h-10" src={productImg} alt="product" />
-          <div>
-            <div className="text-xs leading-[1.4] text-blue2">{product.id}</div>
-            <div className="text-sm leading-[1.5] text-gray7">
-              {product.name}
+        {/* ///////////////////////////////////////////////////////////////// */}
+        <div>
+          <div className="flex items-center gap-2">
+            <img className="h-10" src={productImg} alt="product" />
+            <div>
+              <div className="text-xs leading-[1.4] text-blue2">
+                {product.id}
+              </div>
+              <div className="text-sm leading-[1.5] text-gray7">
+                {product.name}
+              </div>
+            </div>
+          </div>
+
+          <div
+            className={`lg:hidden text-sm leading-[1.5] transition-all duration-300 ease-in-out ${
+              isOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"
+            } space-y-3`}
+          >
+            <div className="flex gap-4 pt-3">
+              <p className="w-15 text-gray2">Price</p>
+              <p className="text-gray1">${product.price.toFixed(2)}</p>
+            </div>
+            <div className="flex gap-4">
+              <p className="w-15 text-gray2">QTY</p>
+              <p className="text-gray1">{product.qty}</p>
+            </div>
+            <div className="flex gap-4">
+              <p className="w-15 text-gray2">Date</p>
+              <p className="text-gray1">{product.date}</p>
+            </div>
+            <div className="flex gap-4">
+              <p className="w-15 text-gray2">Status</p>
+              <div
+                className={`h-7.5 px-2.5 py-1.5 ${
+                  product.status === "Out of Stock"
+                    ? "bg-red2 text-red1"
+                    : "bg-green2 text-green1"
+                } font-medium text-xs leading-[1.4] rounded-[10px]`}
+              >
+                {product.status}
+              </div>
+            </div>
+            <div className="flex gap-4">
+              <p className="w-15 text-gray2">Action</p>
+              <div className="flex gap-4">
+                <svg className="size-6 stroke-gray7 fill-transparent">
+                  <use href="./sprite.svg#icon-eye-open"></use>
+                </svg>
+                <svg className="size-6 stroke-gray7 fill-transparent">
+                  <use href="./sprite.svg#icon-edit"></use>
+                </svg>
+                <svg className="size-6 stroke-gray7 fill-transparent">
+                  <use href="./sprite.svg#icon-delete"></use>
+                </svg>
+              </div>
             </div>
           </div>
         </div>
-
         {/* ///////////////////////////////////////////////////////////////// */}
         <div className="w-30 flex-shrink-0 hidden lg:flex justify-center">
           <p className="product-text">${product.price.toFixed(2)}</p>
@@ -66,69 +115,24 @@ const ProductItem = ({ product }: ProductProps) => {
             <use href="./sprite.svg#icon-delete"></use>
           </svg>
         </div>
+      </div>
+      {/* ///////////////////////////////////////////////////////////////// */}
 
-        <div
-          onClick={() => setIsOpen(!isOpen)}
-          className="w-8 h-7 flex justify-center items-center lg:hidden bg-gray13 rounded-lg"
-        >
-          {!isOpen ? (
-            <svg className="size-5 stroke-gray7 fill-transparent">
-              <use href="./sprite.svg#icon-arrow-down-simple"></use>
-            </svg>
-          ) : (
-            <svg className="size-5 stroke-gray7 fill-transparent">
-              <use href="./sprite.svg#icon-arrow-up-simple"></use>
-            </svg>
-          )}
-        </div>
-      </div>
-      {/* ///////////////////////////////////////////////////////////////// */}
       <div
-        className={`text-sm leading-[1.5] transition-all duration-300 ease-in-out ${
-          isOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"
-        } space-y-3 pl-12 pb-3 -mt-1`}
+        onClick={() => setIsOpen(!isOpen)}
+        className="w-8 h-7 flex justify-center items-center lg:hidden bg-gray13 rounded-lg m-4"
       >
-        <div className="flex gap-4">
-          <p className="w-15 text-gray2">Price</p>
-          <p className="text-gray1">${product.price.toFixed(2)}</p>
-        </div>
-        <div className="flex gap-4">
-          <p className="w-15 text-gray2">QTY</p>
-          <p className="text-gray1">{product.qty}</p>
-        </div>
-        <div className="flex gap-4">
-          <p className="w-15 text-gray2">Date</p>
-          <p className="text-gray1">{product.date}</p>
-        </div>
-        <div className="flex gap-4">
-          <p className="w-15 text-gray2">Status</p>
-          <div
-            className={`h-7.5 px-2.5 py-1.5 ${
-              product.status === "Out of Stock"
-                ? "bg-red2 text-red1"
-                : "bg-green2 text-green1"
-            } font-medium text-xs leading-[1.4] rounded-[10px]`}
-          >
-            {product.status}
-          </div>
-        </div>
-        <div className="flex gap-4">
-          <p className="w-15 text-gray2">Action</p>
-          <div className="flex gap-4">
-            <svg className="size-6 stroke-gray7 fill-transparent">
-              <use href="./sprite.svg#icon-eye-open"></use>
-            </svg>
-            <svg className="size-6 stroke-gray7 fill-transparent">
-              <use href="./sprite.svg#icon-edit"></use>
-            </svg>
-            <svg className="size-6 stroke-gray7 fill-transparent">
-              <use href="./sprite.svg#icon-delete"></use>
-            </svg>
-          </div>
-        </div>
+        {!isOpen ? (
+          <svg className="size-5 stroke-gray7 fill-transparent">
+            <use href="./sprite.svg#icon-arrow-down-simple"></use>
+          </svg>
+        ) : (
+          <svg className="size-5 stroke-gray7 fill-transparent">
+            <use href="./sprite.svg#icon-arrow-up-simple"></use>
+          </svg>
+        )}
       </div>
-      {/* ///////////////////////////////////////////////////////////////// */}
-    </>
+    </div>
   );
 };
 export default ProductItem;
